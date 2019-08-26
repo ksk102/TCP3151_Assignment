ddf54d7829145d8c18d59efb34b061c16748630e
KS Koh <kahsiang1123@outlook.com>
2019-08-27 00:09:02 +0800

    feat(ArrayGenerate): add array generator
    to generate array with given length,m fill with random number from 1 - 100

---
+++ Part2-Java_Native_Interface/Part2_Question2/ArrayGenerate.java
@@ -0,0 +1,77 @@
+import java.util.Scanner;
+import java.io.IOException;
+
+class ArrayGenerate{
+  private native void arrayGenerator(int length);
+
+  private int arrayNum[];
+
+  // To clear the terminal
+  public static void clearScreen(){  
+    try {
+      new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+    } 
+    catch (InterruptedException | IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  // Header line for the CLI program
+  private void displayTitle(){
+    // clear the terminal
+    ArrayGenerate.clearScreen();
+
+    System.out.println("Array Generation");
+    System.out.println("----------------------------------------------------");
+  }
+
+  // get user input as length for array
+  private int inputArrayLength(Scanner scanner){
+    int length;
+    boolean error;
+
+    do{
+      error = false;
+      
+      // get user input as length
+      System.out.print("Size of array to generate: ");
+      try{
+        length = Integer.parseInt(scanner.nextLine());
+      }
+      // if user input is not a number
+      catch(NumberFormatException e){
+        length = 0;
+      }
+
+      // if the length is not valid, ask the user to re-enter again, until a valid input is inserted
+      if(length < 1){
+        System.out.println();
+        System.out.println("Invalid input! Size of array must be equal or larger than 1");
+        error = true;
+      }
+    } while(error);
+
+    return length;
+  }
+
+  public static void main(String[] args){
+    int arrayLength;
+    ArrayGenerate arrayGenerate = new ArrayGenerate();
+    
+    Scanner scanner = new Scanner(System.in);
+    // display the header line
+    arrayGenerate.displayTitle();
+    // get the length of the array from user
+    arrayLength = arrayGenerate.inputArrayLength(scanner);
+    // avoid resource leak
+    scanner.close();
+
+    // generate the array with random numbers
+    arrayGenerate.arrayNum = new int[arrayLength];
+    arrayGenerate.arrayGenerator(arrayLength);
+  }
+
+  static{
+    System.loadLibrary("ArrayGenerator");
+  }
+}
---
+++ Part2-Java_Native_Interface/Part2_Question2/ArrayGenerator.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <jni.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+
+// generate the array with random number [1-100] with given length
+void GenerateArray(jint *theArray, int length){
+  bool numLessThan50 = false;
+
+  printf("Numbers less than 50 in the array: \n");
+
+  for(int i = 0; i < length; i++){
+    // generate the random numbers
+    // (rand() % (MAX - MIN + 1)) + MIN
+    theArray[i] = (rand() % (100 - 1 + 1)) + 1;
+
+    // display the numbers that are less than 50
+    if(theArray[i] < 50){
+      numLessThan50 = true;
+      printf("%ld ", theArray[i]);
+    }
+  }
+  
+  // if no number less than 50
+  if(!numLessThan50){
+    printf("not found");
+  }
+}
+
+JNIEXPORT void JNICALL Java_ArrayGenerate_arrayGenerator(JNIEnv *env, jobject object, jint length){
+  jint randomNumber[length];
+
+  // Use current time as seed for random generator 
+  srand(time(0)); 
+
+  // generate the array
+  GenerateArray(randomNumber, length);
+
+  // link with java class, to get java array field
+  jclass clazz = (*env)->GetObjectClass(env, object);
+  jfieldID fid = (*env)->GetFieldID(env, clazz, "arrayNum", "[I");
+  jintArray javaArray = (*env)->GetObjectField(env, object, fid);
+
+  // copy the native array to java array
+  (*env)->SetIntArrayRegion(env, javaArray, 0, length, randomNumber);
+}
