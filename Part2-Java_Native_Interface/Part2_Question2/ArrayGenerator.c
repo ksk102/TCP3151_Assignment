1fde4eabcceb587cae856ea89cd73ec771350702
KS Koh <kahsiang1123@outlook.com>
2019-09-06 14:47:00 +0800

    feat(ArrayGenerate): display all elements instead of less than 50

--- Part2-Java_Native_Interface/Part2_Question2/ArrayGenerator.c
+++ Part2-Java_Native_Interface/Part2_Question2/ArrayGenerator.c
@@ -5,7 +5,9 @@
 #include <time.h>
 
 // generate the array with random number [1-100] with given length
-void GenerateArray(jint *theArray, int length){
+// amendment to the assignment question @ 20190904
+// display all elements in the array instead of only less than 50
+void GenerateArrayDisplayL50(jint *theArray, int length){
   bool numLessThan50 = false;
 
   printf("Numbers less than 50 in the array: ");
@@ -28,6 +30,30 @@ void GenerateArray(jint *theArray, int length){
   }
 
   printf("\n");
+}
+
+// generate the array with random number [1-100] with given length
+// amendment to the assignment question @ 20190904
+// display all elements in the array instead of only less than 50
+void GenerateArray(jint *theArray, int length){
+  int countLess50 = 0;
+  printf("Numbers in the array: ");
+
+  for(int i = 0; i < length; i++){
+    // generate the random numbers
+    // (rand() % (MAX - MIN + 1)) + MIN
+    theArray[i] = (rand() % (100 - 1 + 1)) + 1;
+    printf("%ld ", theArray[i]);
+
+    // count the number of elements that are less than 50
+    if(theArray[i] < 50){
+      countLess50++;
+    }
+  }
+  printf("\n");
+
+  // display the number of elements less than 50
+  printf("Number of elements less than 50: %d\n", countLess50);
 }
 
 JNIEXPORT jintArray JNICALL Java_ArrayGenerate_arrayGenerator(JNIEnv *env, jobject object, jint length){
